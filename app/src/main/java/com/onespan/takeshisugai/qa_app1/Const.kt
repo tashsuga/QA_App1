1fb38ef400ec3a607e73ee1facd3de683d8ca27a
takeshisugai <tsu.vasco@gmail.com>
2019-01-31 07:27:38 +0900

    Initial commit

---
+++ app/src/main/java/com/onespan/takeshisugai/qa_app1/Const.kt
@@ -0,0 +1,8 @@
+package com.onespan.takeshisugai.qa_app1
+
+//package jp.techacademy.taro.kirameki.qa_app
+
+const val UsersPATH = "users"       // Firebaseにユーザの表示名を保存するパス
+const val ContentsPATH = "contents" // Firebaseに質問を保存するバス
+const val AnswersPATH = "answers"   // Firebaseに解答を保存するパス
+const val NameKEY = "name"          // Preferenceに表示名を保存する時のキー
