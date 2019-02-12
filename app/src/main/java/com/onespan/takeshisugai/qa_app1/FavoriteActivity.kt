c3ca9027d51843504e8755f63bca1fd407616e14
takeshisugai <tsu.vasco@gmail.com>
2019-02-12 17:24:10 +0900

    1/31

--- app/src/main/java/com/onespan/takeshisugai/qa_app1/FavoriteActivity.kt
+++ app/src/main/java/com/onespan/takeshisugai/qa_app1/FavoriteActivity.kt
@@ -10,6 +10,336 @@ import android.support.design.widget.NavigationView
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.util.Base64
+import android.util.Log
+import android.widget.ListView
+
+import com.google.firebase.auth.FirebaseAuth
+
+import kotlinx.android.synthetic.main.activity_favorite.*
+
+
+import com.google.firebase.database.*
+
+class FavoriteActivity : AppCompatActivity()  {
+
+    private lateinit var mToolbar: Toolbar
+    private var mGenre = 0
+
+    private var FavoritePATH = "temp1"
+
+    // --- ここから ---
+    private lateinit var mDatabaseReference: DatabaseReference
+    private lateinit var mListView: ListView
+    private lateinit var mQuestionArrayList_All: ArrayList<Question>      // 2019/02/12 E.Nozaki すべての質問のリスト
+    private lateinit var mQuestionArrayList_Favorite: ArrayList<Question> // 2019/02/12 E.Nozaki お気に入りの質問のリスト
+    private lateinit var mFavoriteQuestionIDs: ArrayList<String>          // 2019/02/12 E.Nozaki お気に入りの質問のIDのリスト
+    private lateinit var mAdapter: QuestionsListAdapter
+
+    private var mGenreRef: DatabaseReference? = null
+
+    private val mEventListener = object : ChildEventListener {
+
+        override fun onCancelled(p0: DatabaseError) {
+            // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onChildMoved(p0: DataSnapshot, p1: String?) {
+            // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onChildRemoved(p0: DataSnapshot) {
+            // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onChildAdded(dataSnapshot: DataSnapshot, s: String?) {
+            val map = dataSnapshot.value as Map<String, String>
+            val title = map["title"] ?: ""
+            val body = map["body"] ?: ""
+            val name = map["name"] ?: ""
+            val uid = map["uid"] ?: ""
+            val imageString = map["image"] ?: ""
+            val bytes =
+                if (imageString.isNotEmpty()) {
+                    Base64.decode(imageString, Base64.DEFAULT)
+                } else {
+                    byteArrayOf()
+                }
+
+            val answerArrayList = ArrayList<Answer>()
+            val answerMap = map["answers"] as Map<String, String>?
+            if (answerMap != null) {
+                for (key in answerMap.keys) {
+                    val temp = answerMap[key] as Map<String, String>
+                    val answerBody = temp["body"] ?: ""
+                    val answerName = temp["name"] ?: ""
+                    val answerUid = temp["uid"] ?: ""
+                    val answer = Answer(answerBody, answerName, answerUid, key)
+                    answerArrayList.add(answer)
+                }
+            }
+
+            val question = Question(
+                title, body, name, uid, dataSnapshot.key ?: "",
+                mGenre, bytes, answerArrayList
+            )
+            mQuestionArrayList_All.add(question) // 2019/02/12 E.Nozaki すべての質問を mQuestionArrayList_All にまとめて格納しておきます。
+            filterFavoriteQuestions()  // 2019/02/12 E.Nozaki mQuestionArrayList_All の中からお気に入りの質問を抽出して、mQuestionArrayList_Favorite に格納します。
+            mAdapter.notifyDataSetChanged()
+
+            Log.d("Eiichi", "mQuestionArrayList_Allのサイズ=" + mQuestionArrayList_All.size + ", 質問=" + question.title)
+        }
+
+        override fun onChildChanged(dataSnapshot: DataSnapshot, s: String?) {
+            val map = dataSnapshot.value as Map<String, String>
+
+            // 変更があったQuestionを探す
+            for (question in mQuestionArrayList_Favorite) { // 2019/02/12 E.Nozaki
+                if (dataSnapshot.key.equals(question.questionUid)) {
+                    // このアプリで変更がある可能性があるのは回答(Answer)のみ
+                    question.answers.clear()
+                    val answerMap = map["answers"] as Map<String, String>?
+                    if (answerMap != null) {
+                        for (key in answerMap.keys) {
+                            val temp = answerMap[key] as Map<String, String>
+                            val answerBody = temp["body"] ?: ""
+                            val answerName = temp["name"] ?: ""
+                            val answerUid = temp["uid"] ?: ""
+                            val answer = Answer(answerBody, answerName, answerUid, key)
+                            question.answers.add(answer)
+                        }
+                    }
+
+                    mAdapter.notifyDataSetChanged()
+                }
+            }
+        }
+
+        /*
+        override fun onChildRemoved(p0: DataSnapshot) {
+
+        }
+
+        override fun onChildMoved(p0: DataSnapshot, p1: String?) {
+
+        }
+
+        override fun onCancelled(p0: DatabaseError) {
+
+        }
+    }
+    :?
+  */
+
+    }
+    ///+++++++++
+
+    // 2019/02/12 E.Nozaki 下記は「お気に入り」の情報を取得するためのリスナーの定義です。
+
+    private val mFavoriteEventListener = object : ChildEventListener {
+
+        override fun onChildAdded(snapshot: DataSnapshot, p1: String?) {
+            if(snapshot.value==true){
+                mFavoriteQuestionIDs.add(snapshot.key.toString())
+            } else {
+                mFavoriteQuestionIDs.remove(snapshot.key.toString())
+            }
+
+            filterFavoriteQuestions()  // 2019/02/12 E.Nozaki mQuestionArrayList_All の中からお気に入りの質問を抽出して、mQuestionArrayList_Favorite に格納します。
+            mAdapter.notifyDataSetChanged()
+        }
+
+        override fun onChildChanged(snapshot: DataSnapshot, p1: String?) {
+            if(snapshot.value==true){
+                mFavoriteQuestionIDs.add(snapshot.key.toString())
+            } else {
+                mFavoriteQuestionIDs.remove(snapshot.key.toString())
+            }
+
+            filterFavoriteQuestions()  // 2019/02/12 E.Nozaki mQuestionArrayList_All の中からお気に入りの質問を抽出して、mQuestionArrayList_Favorite に格納します。
+            mAdapter.notifyDataSetChanged()
+        }
+
+        override fun onChildRemoved(snapshot: DataSnapshot) {
+            mFavoriteQuestionIDs.remove(snapshot.key.toString())
+            filterFavoriteQuestions()  // 2019/02/12 E.Nozaki mQuestionArrayList_All の中からお気に入りの質問を抽出して、mQuestionArrayList_Favorite に格納します。
+            mAdapter.notifyDataSetChanged()
+        }
+
+        override fun onChildMoved(snapshot: DataSnapshot, p1: String?) { }
+        override fun onCancelled(error: DatabaseError) {}
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        lateinit var mQuestion: Question
+
+        // →まず、onCreate関数を作成してください。
+        //　次にその中で、mEventListenerを呼び出す処理を書いてください
+
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite) //これは必要でしょうか？
+
+        // 渡ってきたQuestionのオブジェクトを保持する
+        val extras = intent.extras
+
+
+
+        // activity_favorite.
+        //  super.onCreate(savedInstanceState)
+        //  setContentView(R.layout.activity_favorite) //これは必要でしょうか？
+        //  setContentView(R.layout.activity_main)
+
+        // deleted 1oth/Feb.
+        // mToolbar = findViewById(R.id.toolbar)
+        // setSupportActionBar(mToolbar)
+
+
+        /*
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+
+
+        fab.setOnClickListener { view ->
+            // ジャンルを選択していない場合（mGenre == 0）はエラーを表示するだけ
+            if (mGenre == 0) {
+                //Snackbar.make(view, "ジャンルを選択して下さい", Snackbar.LENGTH_LONG).show()
+            } else {
+
+            }
+            // ログイン済みのユーザーを取得する
+            val user = FirebaseAuth.getInstance().currentUser
+
+            if (user == null) {
+                // ログインしていなければログイン画面に遷移させる
+                val intent = Intent(applicationContext, LoginActivity::class.java)
+                startActivity(intent)
+            } else {
+                // ジャンルを渡して質問作成画面を起動する
+                val intent = Intent(applicationContext, QuestionSendActivity::class.java)
+                intent.putExtra("genre", mGenre)
+                startActivity(intent)
+            }
+        }
+  */
+        // ナビゲーションドロワーの設定
+        /*
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val toggle = ActionBarDrawerToggle(this, drawer, mToolbar, R.string.app_name, R.string.app_name)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+     */
+
+        //   val navigationView = findViewById<NavigationView>(R.id.nav_view)
+
+
+        // 9th/Fev
+        // navigationView.NavigationItemSelectedListener(this)
+
+        // Firebase
+        mDatabaseReference = FirebaseDatabase.getInstance().reference
+
+        // 2019/02/12 下記の3つのリストを初期化します。
+        mQuestionArrayList_All = ArrayList<Question>()      // 2019/02/12 E.Nozaki
+        mQuestionArrayList_Favorite = ArrayList<Question>() // 2019/02/12 E.Nozaki
+        mFavoriteQuestionIDs = ArrayList<String>()          // 2019/02/12 E.Nozaki
+
+        // ListViewの準備
+        mListView = findViewById(R.id.listView)
+        mAdapter = QuestionsListAdapter(this)
+        mAdapter.setQuestionArrayList(mQuestionArrayList_Favorite) // 2019/02/12 E.Nozaki
+        mListView.adapter = mAdapter // 2019/02/12 E.Nozaki
+        mAdapter.notifyDataSetChanged()
+
+        mListView.setOnItemClickListener { parent, view, position, id ->
+            // Questionのインスタンスを渡して質問詳細画面を起動する
+            val intent = Intent(applicationContext, QuestionDetailActivity::class.java)
+            intent.putExtra("question", mQuestionArrayList_Favorite[position]) // 2019/02/12 E.Nozaki
+            startActivity(intent)
+        }
+
+        /* 10th added by T. Sugai  */
+
+        /*
+        // --- ここから ---
+        // 質問のリストをクリアしてから再度Adapterにセットし、AdapterをListViewにセットし直す
+        mQuestionArrayList.clear()
+        mAdapter.setQuestionArrayList(mQuestionArrayList)
+        mListView.adapter = mAdapter
+
+        // 選択したジャンルにリスナーを登録する
+        if (mGenreRef != null) {
+            mGenreRef!!.removeEventListener(mEventListener)
+        }
+        mGenreRef = mDatabaseReference.child(ContentsPATH).child(mGenre.toString())
+        mGenreRef!!.addChildEventListener(mEventListener)
+        // --- ここまで追加する ---
+*/
+
+        // ログイン済みのユーザーを取得する
+        val user = FirebaseAuth.getInstance().currentUser
+
+        if (user == null) {
+            // ログインしていなければログイン画面に遷移させる
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+        } else {
+
+            // 2019/02/11 E.Nozaki このユーザーがこの質問を「お気に入りに登録したかどうか」をセットしておく Firebase上の場所にリスナーを設定します。
+
+            mDatabaseReference.child(FavoritePATH).child(user.uid).addChildEventListener(mFavoriteEventListener)
+
+            // 2019/02/12 E.Nozaki すべてのジャンルの質問を Firebase から取得します。
+
+            mDatabaseReference.child(ContentsPATH).child("1").addChildEventListener(mEventListener)
+            mDatabaseReference.child(ContentsPATH).child("2").addChildEventListener(mEventListener)
+            mDatabaseReference.child(ContentsPATH).child("3").addChildEventListener(mEventListener)
+            mDatabaseReference.child(ContentsPATH).child("4").addChildEventListener(mEventListener)
+
+            // ジャンルを渡して質問作成画面を起動する
+            //val intent = Intent(applicationContext, QuestionSendActivity::class.java)
+            //intent.putExtra("genre", mGenre)
+            //startActivity(intent)
+        }
+
+        // adviced by Mr. Hori
+        // mGenreRef = mDatabaseReference.child(FavoritePATH).child(mGenreRef.toString())
+        // mGenreRef = mDatabaseReference.child(FavoritePATH).child(user!!.uid).child(mQuestion.uid)
+        // mGenreRef!!.addChildEventListener(mEventListener)
+
+        // .child(user.uid)
+    }
+
+    // 2019/02/12 E.Nozaki 下記は、mQuestionArrayList_All の中からお気に入りの質問を抽出して、mQuestionArrayList_Favorite に格納するメソッドの定義です。
+
+    private fun filterFavoriteQuestions() {
+
+        mQuestionArrayList_Favorite.clear()
+
+        for(question in mQuestionArrayList_All) {
+
+            if(mFavoriteQuestionIDs.contains(question.uid)) { // 2019/02/12 E.Nozaki この質問が「お気に入り」に登録されているかどうかをチェック。
+                mQuestionArrayList_Favorite.add(question)
+            }
+        }
+    }
+}
+
+
+
+/*
+package com.onespan.takeshisugai.qa_app1
+
+//package jp.techacademy.taro.kirameki.qa_app
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.NavigationView
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.util.Base64
 import android.widget.ListView
 
 import com.google.firebase.auth.FirebaseAuth
@@ -108,21 +438,20 @@ class FavoriteActivity : AppCompatActivity()  {
             }
         }
 
-        /*
-        override fun onChildRemoved(p0: DataSnapshot) {
 
-        }
+        // override fun onChildRemoved(p0: DataSnapshot) {
+      //
+       // }
+      //
+      //  override fun onChildMoved(p0: DataSnapshot, p1: String?) {
+     //
+     //   }
+     //
+     //   override fun onCancelled(p0: DatabaseError) {
+//
+//        }
+   // }
 
-        override fun onChildMoved(p0: DataSnapshot, p1: String?) {
-
-        }
-
-        override fun onCancelled(p0: DatabaseError) {
-
-        }
-    }
-    :?
-  */
 
     }
     ///+++++++++
@@ -154,39 +483,7 @@ class FavoriteActivity : AppCompatActivity()  {
         // setSupportActionBar(mToolbar)
 
 
-        /*
-        val fab = findViewById<FloatingActionButton>(R.id.fab)
 
-
-        fab.setOnClickListener { view ->
-            // ジャンルを選択していない場合（mGenre == 0）はエラーを表示するだけ
-            if (mGenre == 0) {
-                //Snackbar.make(view, "ジャンルを選択して下さい", Snackbar.LENGTH_LONG).show()
-            } else {
-
-            }
-            // ログイン済みのユーザーを取得する
-            val user = FirebaseAuth.getInstance().currentUser
-
-            if (user == null) {
-                // ログインしていなければログイン画面に遷移させる
-                val intent = Intent(applicationContext, LoginActivity::class.java)
-                startActivity(intent)
-            } else {
-                // ジャンルを渡して質問作成画面を起動する
-                val intent = Intent(applicationContext, QuestionSendActivity::class.java)
-                intent.putExtra("genre", mGenre)
-                startActivity(intent)
-            }
-        }
-  */
-        // ナビゲーションドロワーの設定
-        /*
-        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
-        val toggle = ActionBarDrawerToggle(this, drawer, mToolbar, R.string.app_name, R.string.app_name)
-        drawer.addDrawerListener(toggle)
-        toggle.syncState()
-     */
 
      //   val navigationView = findViewById<NavigationView>(R.id.nav_view)
 
@@ -210,23 +507,7 @@ class FavoriteActivity : AppCompatActivity()  {
             startActivity(intent)
         }
 
-     /* 10th added by T. Sugai  */
 
-        /*
-        // --- ここから ---
-        // 質問のリストをクリアしてから再度Adapterにセットし、AdapterをListViewにセットし直す
-        mQuestionArrayList.clear()
-        mAdapter.setQuestionArrayList(mQuestionArrayList)
-        mListView.adapter = mAdapter
-
-        // 選択したジャンルにリスナーを登録する
-        if (mGenreRef != null) {
-            mGenreRef!!.removeEventListener(mEventListener)
-        }
-        mGenreRef = mDatabaseReference.child(ContentsPATH).child(mGenre.toString())
-        mGenreRef!!.addChildEventListener(mEventListener)
-        // --- ここまで追加する ---
-*/
 
         // ログイン済みのユーザーを取得する
         val user = FirebaseAuth.getInstance().currentUser
@@ -259,3 +540,4 @@ class FavoriteActivity : AppCompatActivity()  {
 
 
 }
+        */
--- app/src/main/java/com/onespan/takeshisugai/qa_app1/QuestionDetailActivity.kt
+++ app/src/main/java/com/onespan/takeshisugai/qa_app1/QuestionDetailActivity.kt
@@ -1,4 +1,4 @@
-
+/*
 
 package com.onespan.takeshisugai.qa_app1
 
@@ -178,6 +178,183 @@ class QuestionDetailActivity : AppCompatActivity() {
 
 
 }
+*/
+
+package com.onespan.takeshisugai.qa_app1
+
+//package jp.techacademy.taro.kirameki.qa_app
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import android.widget.ListView
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_question_detail.*
+
+import java.util.HashMap
+
+class QuestionDetailActivity : AppCompatActivity() {
+
+    private lateinit var mQuestion: Question
+    private lateinit var mAdapter: QuestionDetailListAdapter
+    private lateinit var mAnswerRef: DatabaseReference
+    private var isFavorite = false // 2019/02/11 E.Nozaki ここに「お気に入りになっているかどうかの状態」を定義しておきます。初期値は「お気に入り解除」とします。なので、falseをセットしておきます。
+
+    // private var FavoritePATH = "./temp1"
+    private var FavoritePATH = "temp1"
+
+
+    private val mEventListener = object : ChildEventListener {
+        override fun onChildAdded(dataSnapshot: DataSnapshot, s: String?) {
+            val map = dataSnapshot.value as Map<String, String>
+
+            val answerUid = dataSnapshot.key ?: ""
+
+            for (answer in mQuestion.answers) {
+                // 同じAnswerUidのものが存在しているときは何もしない
+                if (answerUid == answer.answerUid) {
+                    return
+                }
+            }
+
+            val body = map["body"] ?: ""
+            val name = map["name"] ?: ""
+            val uid = map["uid"] ?: ""
+
+            val answer = Answer(body, name, uid, answerUid)
+            mQuestion.answers.add(answer)
+            mAdapter.notifyDataSetChanged()
+        }
+
+        override fun onChildChanged(dataSnapshot: DataSnapshot, s: String?) {
+
+        }
+
+        override fun onChildRemoved(dataSnapshot: DataSnapshot) {
+
+        }
+
+        override fun onChildMoved(dataSnapshot: DataSnapshot, s: String?) {
+
+        }
+
+        override fun onCancelled(databaseError: DatabaseError) {
+
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question_detail)
+
+        // 渡ってきたQuestionのオブジェクトを保持する
+        val extras = intent.extras
+        mQuestion = extras.get("question") as Question
+
+        title = mQuestion.title
+
+        // ListViewの準備
+        mAdapter = QuestionDetailListAdapter(this, mQuestion)
+        listView.adapter = mAdapter
+        mAdapter.notifyDataSetChanged()
+
+        fab.setOnClickListener {
+            // ログイン済みのユーザーを取得する
+            val user = FirebaseAuth.getInstance().currentUser
+
+            if (user == null) {
+                // ログインしていなければログイン画面に遷移させる
+                val intent = Intent(applicationContext, LoginActivity::class.java)
+                startActivity(intent)
+            } else {
+                // Questionを渡して回答作成画面を起動する
+                // --- ここから ---
+                val intent = Intent(applicationContext, AnswerSendActivity::class.java)
+                intent.putExtra("question", mQuestion)
+                startActivity(intent)
+                // --- ここまで ---
+            }
+        }
+
+        val dataBaseReference = FirebaseDatabase.getInstance().reference
+        mAnswerRef =
+            dataBaseReference.child(ContentsPATH).child(mQuestion.genre.toString()).child(mQuestion.questionUid)
+                .child(AnswersPATH)
+        mAnswerRef.addChildEventListener(mEventListener)
+
+        // 2019/02/11 E.Nozaki ボタンに表示するテキストの初期値は、「お気に入り追加」の状態にしておきます。
+
+        button2.text = "お気に入りに追加" // 2019/02/11 E.Nozaki ボタン上のテキストの初期値をセットしておきます。
+
+        // 2019/02/11 E.Nozaki 下記は、ユーザーがログインしている場合に動作する「お気に入り」の状態を操作するコードです。
+
+        val user = FirebaseAuth.getInstance().currentUser  // ユーザー取得
+
+        if (user != null) {
+
+            // 2019/02/11 E.Nozaki 下記に、このユーザーがこの質問を「お気に入りに登録したかどうか」をセットしておく Firebase上の場所の定義です。
+
+            val favoriteRef = FirebaseDatabase
+                .getInstance()
+                .reference
+                .child(FavoritePATH)
+                .child(user.uid)
+                .child(mQuestion.uid)
+
+            // 2019/02/11 E.Nozaki 下記に、「お気に入り」ボタンが押された時に呼び出される処理をセットしておきます。
+
+            this.button2.setOnClickListener {
+
+                val user = FirebaseAuth.getInstance().currentUser  // ユーザー取得
+
+                if (user != null) {
+                    if(isFavorite) {
+                        // 2019/02/11 E.Nozaki もし、すでにお気に入りに登録済みであれば、お気に入りを解除する
+                        favoriteRef.setValue(false)
+                    } else {
+                        // 2019/02/11 E.Nozaki もし、お気に入りに登録済みでなければ、お気に入りに追加する
+                        favoriteRef.setValue(true)
+                    }
+                }
+            }
+
+            // 2019/02/11 E.Nozaki この質問が、Firebase上でお気に入りとして登録されたり、解除されたりした時に呼び出されるリスナーです。
+
+            val favoriteListener = object : ValueEventListener {
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+
+                    val value = snapshot.getValue()
+
+                    if(value==true) {
+                        Log.d("Eiichi", "お気に入りに登録されました！")
+                        isFavorite = true // 2019/02/11 E.Nozaki お気に入りに登録されました！
+                        button2.text = "お気に入り解除" // 2019/02/11 E.Nozaki お気に入りに登録されたので、今度は解除できように画面上の表示を変えておきます。
+                    } else { // 2019/02/12 E.Nozaki
+                        Log.d("Eiichi", "お気に入り解除されました。")
+                        isFavorite = false // 2019/02/11 E.Nozaki お気に入り解除されました。
+                        button2.text = "お気に入り登録" // 2019/02/11 E.Nozaki お気に入り解除されたので、今度は登録できように画面上の表示を変えておきます。
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) { }
+            }
+
+            // 2019/02/11 E.Nozaki 下記では、上記で作成したリスナーを Firebase上の「お気に入りに登録したかどうか」の場所にセットして、常時監視するようにしています。
+
+            favoriteRef.addValueEventListener(favoriteListener)
+        }
+    }
+}
+
+
 
 
 /*
